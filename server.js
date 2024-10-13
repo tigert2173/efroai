029fb26d2d9d630d20d714bcba98eccd9988d262
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-10-13 16:21:36 -0400

    s

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
-const cors = request('cors')
+const cors = require('cors')
 const path = require('path');
 
 const app = express();
