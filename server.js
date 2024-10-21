ed4a62a885627f7c012044c2dd21a31191e900bd
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-10-21 17:07:46 -0400

    t

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
     }
 
     // Check if the request is for the waitlist page
-    if (req.path === '/images/logotransparent.jpg') {
+    if (req.path === '/images/logotransparent.png') {
       // Allow access to the waitlist page without counting the user as active
       return next();
     }
