fbb9022e4924ece953ed2c97f4124fa95ed60cb8
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-10-21 16:15:26 -0400

    t

--- server.js
+++ server.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
-const cors = require('cors')
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
+const maxConcurrentUsers = 1; // Maximum concurrent users
+let currentUsers = 0; // Track current active users
+
 // Use CORS middleware
 app.use(cors());
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'docs')));
+
+// Middleware to handle concurrent user limit
+app.use((req, res, next) => {
+  if (currentUsers >= maxConcurrentUsers) {
+    // Redirect to waitlist page
+    return res.redirect('/capacity/capacity.html');
+  }
+  
+  // Increment the current user count
+  currentUsers++;
+  
+  // Set up a response interceptor to decrement the count on response end
+  res.on('finish', () => {
+    currentUsers--;
+  });
+  
+  next();
+});
 
 // Read your SSL certificate and private key
 const options = {
