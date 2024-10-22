fe1ad84705b2a0afbb4537640ea173a6c395e0d6
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-10-22 13:41:37 -0400

    t

--- server.js
+++ server.js
@@ -21,7 +21,7 @@
   // Use compression middleware with Brotli support
 app.use(compression({
   // Custom options for Brotli compression
-  level: 1, // Compression level (0-11), 11 is maximum
+  level: 11, // Compression level (0-11), 11 is maximum
 }));
 
   //Add compression middleware with Brotli support
