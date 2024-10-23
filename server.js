0469243b738ff05969340b260b084dba72b8b1d1
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-10-23 15:38:06 -0400

    t

--- server.js
+++ server.js
@@ -16,7 +16,7 @@
     '::ffff:128.14.174.186', //Path: /showLogin.cc & /api/session/properties & /solr/ && /login.do <<-- suspicious request 
     '::ffff:128.14.173.116', //Path: /sugar_version.json && Path: /cgi-bin/authLogin.cgi && Path: /WebInterface/ <<-- suspicious request 
     '::ffff:128.14.173.114', //Path: /cgi-bin/config.exp && Path: /owa/ && /admin/ <<-- suspicious request 
-    '::ffff:69.174.135.234',
+    '69.174.135.234',
     '::ffff:204.110.223.51' // SchoScure
 ]; 
 
@@ -59,9 +59,9 @@ app.use(compression({
   app.use((req, res, next) => {
     // Get user's IP address
     
-    const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // Check headers or socket
-   // const forwarded = req.headers['cf-connecting-ip'];
-    //const userIp = forwarded || req.connection.remoteAddress;
+    //const userIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress; // Check headers or socket
+    const forwarded = req.headers['cf-connecting-ip'];
+    const userIp = forwarded || req.connection.remoteAddress;
 
     console.log(`Incoming request from IP: ${userIp}`);
 
