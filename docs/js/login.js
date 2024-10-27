6e117880b567a0f54ae5b3f9a539f3be0bac1f6c
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-10-27 15:34:06 -0400

    t

--- docs/js/login.js
+++ docs/js/login.js
@@ -36,12 +36,14 @@ function loginUser() {
          // Check the referrer
         const redirectUrl = document.referrer ? document.referrer : '../index.html';
         console.log('Redirecting to:', redirectUrl); // Log the redirect URL
-
+        
+        if (redirectUrl == "../index.html") {
+            window.opener.postMessage({ type: 'login', token: data.token }, window.location.origin); // Send token to the opener
+        }
         // Redirect to the referrer or homepage
         //const redirectUrl = document.referrer || '../index.html';
         window.opener.location.href = redirectUrl; // Change the opener's location
-        window.opener.postMessage({ type: 'login', token: data.token }, window.location.origin); // Send token to the opener
-
+        
         window.close(); // Optionally close the login window
 
     })
