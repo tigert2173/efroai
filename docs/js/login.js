b2c5f94c81c15d29895d9501c68d8eea1fdb74ad
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-10-27 15:32:28 -0400

    t

--- docs/js/login.js
+++ docs/js/login.js
@@ -28,14 +28,18 @@ function loginUser() {
     .then(data => {
         sessionStorage.setItem('token', data.token); // Store token in sessionStorage
           // Store user ID in a cookie
-          document.cookie = `userID=${username}; path=/;`;
+        document.cookie = `userID=${username}; path=/;`;
 
         // Send a message back to the original page
       //  window.opener.postMessage('loggedIn', window.location.origin);
         window.opener.postMessage({ type: 'login', token: data.token }, window.location.origin); // Send token to the opener
 
+         // Check the referrer
+        const redirectUrl = document.referrer ? document.referrer : '../index.html';
+        console.log('Redirecting to:', redirectUrl); // Log the redirect URL
+        
         // Redirect to the referrer or homepage
-        const redirectUrl = document.referrer || '../index.html';
+        //const redirectUrl = document.referrer || '../index.html';
         window.opener.location.href = redirectUrl; // Change the opener's location
         
         window.close(); // Optionally close the login window
