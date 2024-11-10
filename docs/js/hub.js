891da6593fe3deaf5222b25b5272b981a85024ca
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-11-10 13:30:37 -0500

    t

--- docs/js/hub.js
+++ docs/js/hub.js
@@ -69,8 +69,8 @@ function loadCharacters() {
         })
         .then(data => {
             totalCharacters = data.total;
-            adExempt = isAdExempt(userToken); // Check if the user is Ad-Exempt
             const userToken = localStorage.getItem('token'); // Replace with your method of obtaining the token
+            adExempt = isAdExempt(userToken); // Check if the user is Ad-Exempt
             displayCharacters(data.characters, searchQuery);
 
             // Update the received character identifiers with the new ones
