c9efa5d8f9f9b8c810e1bc612e7d711dc816b2b8
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-11-16 17:04:47 -0500

    t

--- docs/js/chatbeta.js
+++ docs/js/chatbeta.js
@@ -1191,7 +1191,7 @@ function displayMessage(content, sender, isFinal = false, isLoading = false) {
         <span class="message-content">${sanitizedContent}</span>
         <button class="edit-btn" onclick="enableEditMode(this, ${messages.length})">Edit</button>
         <button class="delete-btn" onclick="deleteMessage(${messages.length})">Delete</button>
-            <button class="audio-btn" onclick="speakMessage('${messages[currentBotMessageIndex].text}')">Send to Audio</button>
+            <button class="audio-btn" onclick="speakMessage('${messages[currentBotMessageIndex]}')">Send to Audio</button>
         `;
         }
         // If the message is final, update the navigation header
@@ -1214,7 +1214,7 @@ function displayMessage(content, sender, isFinal = false, isLoading = false) {
             <span class="nav-arrows ${currentBotMessageIndex === 0 ? 'disabled' : ''}" onclick="navigateBotMessages(-1)">&#9664;</span>
             <span class="nav-arrows ${currentBotMessageIndex === botMessages.length - 1 ? 'disabled' : ''}" onclick="navigateBotMessages(1)">&#9654;</span>
             <button class="delete-btn" onclick="deleteMessage(${messages.length})">Delete</button>
-            <button class="audio-btn" onclick="speakMessage('${messages[currentBotMessageIndex].text}')">Send to Audio</button>
+            <button class="audio-btn" onclick="speakMessage('${messages[currentBotMessageIndex]}')">Send to Audio</button>
             `;
 
             // Append message header to the chat container
