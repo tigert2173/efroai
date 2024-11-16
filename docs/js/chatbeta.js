629c6d372c87e97c45cffbe746060518cf3a95f6
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-11-16 17:51:10 -0500

    t

--- docs/js/chatbeta.js
+++ docs/js/chatbeta.js
@@ -1194,7 +1194,7 @@ function displayMessage(content, sender, isFinal = false, isLoading = false) {
         <span class="message-content">${sanitizedContent}</span>
         <button class="edit-btn" onclick="enableEditMode(this, ${messages.length})">Edit</button>
         <button class="delete-btn" onclick="deleteMessage(${messages.length})">Delete</button>
-        <button class="audio-btn" onclick="speakMessage(${currentBotMessageElement})">Send to Audio</button>
+        <button class="audio-btn" onclick="speakMessage(${sanitizedContent})">Send to Audio</button>
         `;
         }
         // If the message is final, update the navigation header
@@ -1219,7 +1219,7 @@ function displayMessage(content, sender, isFinal = false, isLoading = false) {
             <button class="edit-btn" onclick="enableEditMode(this, ${messages.length})">Edit</button>
              <button class="delete-btn" onclick="deleteMessage(${messages.length})">Delete</button>
 
-            <button class="audio-btn" onclick="speakMessage(encodeURIComponent(${currentBotMessageElement}))">Send to Audio</button>
+            <button class="audio-btn" onclick="speakMessage(encodeURIComponent(${sanitizedContent}))">Send to Audio</button>
 
             `;
 
