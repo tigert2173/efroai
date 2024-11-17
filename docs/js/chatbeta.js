7b4cbad02c4f579707848560cc484e495f3d847a
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-11-16 19:09:03 -0500

    t

--- docs/js/chatbeta.js
+++ docs/js/chatbeta.js
@@ -1035,7 +1035,7 @@ function speakMessage(index) {
      let tempSentence = '';
  
      sentences.forEach((sentence, index) => {
-         if (sentence.length < 14 && index < sentences.length - 1) {
+         if (sentence.length < 24 && index < sentences.length - 1) {
              // Merge short sentence with the next sentence
              tempSentence += sentence + ' ';
          } else {
