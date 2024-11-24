38a9182d0fffc7c6a19e4cf9cf0c8e5b5e01b727
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2024-11-24 14:01:36 -0500

    s

--- docs/js/chatbeta.js
+++ docs/js/chatbeta.js
@@ -1282,9 +1282,11 @@ function displayMessage(content, sender, isFinal = false, isLoading = false) {
             messageHeader.innerHTML = `
             <span class="nav-arrows ${currentBotMessageIndex === 0 ? 'disabled' : ''}" onclick="navigateBotMessages(-1)">&#9664;</span>
             <span class="nav-arrows ${currentBotMessageIndex === botMessages.length - 1 ? 'disabled' : ''}" onclick="navigateBotMessages(1)">&#9654;</span>
-            <button class="edit-btn" onclick="enableEditMode(this, ${messages.length})">Edit</button>
-            <button class="delete-btn" onclick="deleteMessage(${messages.length})">Delete</button>
-            <button class="audio-btn" onclick="speakMessage(${messages.length})">Send to Audio</button>
+            <div class="button-container">
+                <button class="edit-btn" onclick="enableEditMode(this, ${messages.length})">Edit</button>
+                <button class="delete-btn" onclick="deleteMessage(${messages.length})">Delete</button>
+                <button class="audio-btn" onclick="speakMessage(${messages.length})">Send to Audio</button>
+            </div>
             `;
 
             // Append message header to the chat container
