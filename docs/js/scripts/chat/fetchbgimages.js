ca586b1d4e5f8bf7f5cd35750fd99ac1022007aa
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2025-01-02 13:16:34 -0500

    s

--- docs/js/scripts/chat/fetchbgimages.js
+++ docs/js/scripts/chat/fetchbgimages.js
@@ -204,7 +204,6 @@ document.addEventListener('click', (event) => {
     if (clickedImage && clickedImage.classList.contains('image-slot')) {
         const slot = clickedImage.getAttribute('data-slot'); // Get the slot number
         let direction = event.clientX < window.innerWidth / 2 ? 'prev' : 'next'; // Determine the direction based on click position
-        let attempts = 0;
 
         // If SFW mode is on, restrict the slot range to 1-3
         if (isSFWModeEnabled()) {
