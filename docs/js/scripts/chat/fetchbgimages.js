615272899f630282ad6e8ba4efe0cf03bbde6991
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2025-01-02 15:59:05 -0500

    s

--- docs/js/scripts/chat/fetchbgimages.js
+++ docs/js/scripts/chat/fetchbgimages.js
@@ -44,7 +44,7 @@ document.getElementById('fetchImageForm').addEventListener('submit', async (e) =
             // Apply the opacity to the background using the custom property
             messageElements.forEach(message => {
                 // Update the custom property for background opacity
-                message.style.setProperty('--bg-opacity', document.getElementById('opacity-slider').value);
+                message.style.setProperty('--bg-opacity', document.getElementById('opacity-slider').value/100);
             });
 
 
@@ -205,7 +205,7 @@ async function setImage(slot) {
            // Apply the opacity to the background using the custom property
            messageElements.forEach(message => {
                // Update the custom property for background opacity
-               message.style.setProperty('--bg-opacity', document.getElementById('opacity-slider').value);
+               message.style.setProperty('--bg-opacity', document.getElementById('opacity-slider').value/100);
            });
         // chatWrapper.style.setProperty('--background-image', `url('${url}')`);
         // chatWrapper.style.setProperty('--bg-opacity', 1);
