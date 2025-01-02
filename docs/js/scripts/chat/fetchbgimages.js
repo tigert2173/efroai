391bd3ea943f77bf0de2d8670b74c2e4251b2c85
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2025-01-02 16:05:20 -0500

    s

--- docs/js/scripts/chat/fetchbgimages.js
+++ docs/js/scripts/chat/fetchbgimages.js
@@ -44,7 +44,7 @@ document.getElementById('fetchImageForm').addEventListener('submit', async (e) =
             // Apply the opacity to the background using the custom property
             messageElements.forEach(message => {
                 // Update the custom property for background opacity
-                message.style.setProperty('--bg-opacity', document.getElementById('opacity-slider').value/100);
+                message.style.setProperty('--bg-opacity', document.getElementById('opacity-slider-messages').value/100);
             });
 
 
@@ -88,15 +88,6 @@ document.getElementById('fetchImageForm').addEventListener('submit', async (e) =
     }
 });
 
-
-
-  // Get the slider element
-  const opacitySlider = document.getElementById('opacity-slider');
-
-  // Function to update the label with the slider value
-  opacitySlider.addEventListener('input', function() {
-      const sliderValue = opacitySlider.value;
-  });
 
 // Handle opacity slider
 document.getElementById('opacity-slider').addEventListener('input', (e) => {
@@ -214,7 +205,7 @@ async function setImage(slot) {
            // Apply the opacity to the background using the custom property
            messageElements.forEach(message => {
                // Update the custom property for background opacity
-               message.style.setProperty('--bg-opacity', document.getElementById('opacity-slider').value/100);
+               message.style.setProperty('--bg-opacity', document.getElementById('opacity-slider-messages').value/100);
            });
         // chatWrapper.style.setProperty('--background-image', `url('${url}')`);
         // chatWrapper.style.setProperty('--bg-opacity', 1);
