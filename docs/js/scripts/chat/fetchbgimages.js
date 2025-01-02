036665502c270b8824b3725f61e5c72a31dafab8
tigert2173 <130621909+tigert2173@users.noreply.github.com>
2025-01-02 13:10:01 -0500

    s

--- docs/js/scripts/chat/fetchbgimages.js
+++ docs/js/scripts/chat/fetchbgimages.js
@@ -1,13 +1,12 @@
 // Handle form submission to fetch and set the background or side image
 document.getElementById('fetchImageForm').addEventListener('submit', async (e) => {
     e.preventDefault();
-    let attempts = 0;
     do {
         currentSlot = currentSlot <= (isSFW ? 3 : 10) ? currentSlot : (isSFW ? 1 : 1); // Default to slot 1 or 3 for SFW
         attempts++;
     } while (unavailableSlots.has(currentSlot) && attempts < 10); // Skip unavailable slots
 
-
+    setImage(currentSlot);
     const userId = sessionStorage.getItem('characterUploader'); // Dynamic user ID from character data
     const charId = sessionStorage.getItem('selectedCharacterId'); // Dynamic character ID from character data
     const objectKey = "slot1.webp"; //document.getElementById('objectKey').value.trim();
